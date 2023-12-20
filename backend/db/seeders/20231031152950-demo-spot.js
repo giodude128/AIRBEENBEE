7747d61554a3c19554be2baa66287cc3ae3a64b6
Your Name <you@example.com>
2023-12-19 21:10:35 -0800

    255 char

--- backend/db/seeders/20231031152950-demo-spot.js
+++ backend/db/seeders/20231031152950-demo-spot.js
@@ -20,7 +20,7 @@ module.exports = {
         lat: 23.56,
         lng: 51.24,
         name: 'Family Home',
-        description: 'Welcome to our charming family home nestled in a tranquil neighborhood! This spacious and inviting three-bedroom property boasts a fully equipped kitchen, a cozy living room perfect for family gatherings, and a lush backyard with a barbecue area ideal for outdoor fun. Conveniently located near parks, shops, and local attractions, our home offers the perfect retreat for families looking to create lasting memories together.',
+        description: 'Charming 3-bed home in serene area, featuring fully equipped kitchen, cozy living room, and lush backyard with BBQ. Ideal for family gatherings, near parks, shops, and attractions for a memorable retreat.',
         price: 500,
         previewImage: 'https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/home-improvement/wp-content/uploads/2022/07/download-23.jpg'
       },
@@ -34,7 +34,7 @@ module.exports = {
         lat: 27.21,
         lng: 54.57,
         name: 'Quiet area',
-        description: 'Escape to tranquility in this expansive 10-bedroom residence nestled in a serene and secluded neighborhood. With ample space for relaxation and privacy, this retreat offers a peaceful haven away from the hustle and bustle, boasting spacious rooms, charming communal areas, and a serene garden ideal for unwinding. Experience the luxury of seclusion while still being conveniently close to nearby attractions, making this the perfect spot for a tranquil getaway',
+        description: 'Secluded 10-bed sanctuary in serene locale, featuring spacious rooms, charming communal areas, and a tranquil garden. Enjoy privacy amidst nearby attractions, an ideal haven for a luxurious, peaceful getaway.',
         price: 600,
         previewImage: 'https://www.jamesedition.com/stories/wp-content/uploads/2022/03/mansions_main_fin.jpg'
       },
@@ -47,7 +47,7 @@ module.exports = {
         lat: 31.5,
         lng: 38.4,
         name: 'Party House',
-        description: 'Step into the ultimate party haven at our vibrant and spacious residence designed for unforgettable celebrations! This five-bedroom home features a lively entertainment area complete with a state-of-the-art sound system, a game room equipped with a pool table, and a private outdoor patio perfect for hosting lively gatherings. Embrace the lively atmosphere of this party-centric space, where vibrant nightlife, trendy bars, and local hotspots are just around the corner.',
+        description: 'Vibrant 5-bed party house with entertainment area, top-notch sound system, game room, and private patio for lively gatherings. Perfect for embracing a vibrant atmosphere with nearby nightlife and trendy hotspots.',
         price: 500,
         previewImage: 'https://mygate.com/wp-content/uploads/2023/07/110.jpg'
       },
@@ -60,7 +60,7 @@ module.exports = {
         lat: 21.23,
         lng: 44.67,
         name: 'Haunted House',
-        description: 'Welcome to a spine-tingling adventure at our haunted manor, where history and mystery converge! This eerie five-bedroom estate boasts dark legends, ghostly tales, and original period décor that will send shivers down your spine. Experience an otherworldly stay with nightly tours, paranormal activities, and a truly haunting atmosphere that promises an unforgettable, hair-raising experience for the bravest of souls.',
+        description: 'Experience spine-tingling chills at our haunted 5-bed manor, steeped in dark legends and eerie period décor. Offering ghostly tours, paranormal activities, and a chilling atmosphere for an unforgettable, hair-raising adventure.',
         price: 300,
         previewImage: 'https://cdn.captivatinghouses.com/wp-content/uploads/2023/06/IMG_1543.jpeg'
       },
@@ -73,7 +73,7 @@ module.exports = {
         lat: 27.23,
         lng: 55.67,
         name: 'Beach House',
-        description: 'Indulge in the ultimate coastal retreat at our exquisite beachfront oasis! This charming three-bedroom house offers stunning panoramic views of the ocean, direct beach access, and a spacious sun-drenched patio perfect for savoring breathtaking sunsets. Immerse yourself in the soothing sounds of the waves, enjoy water sports just steps away, and create unforgettable seaside memories in this idyllic haven.',
+        description: 'Immerse in a coastal paradise! 3-bed beachfront house with panoramic ocean views, direct beach access, and a sun-drenched patio for stunning sunsets. Experience seaside tranquility, water sports, and create cherished memories in this idyllic haven.',
         price: 1000,
         previewImage: 'https://hips.hearstapps.com/hmg-prod/images/cayman-islands-villa-kempa-kai-2020-021-1616076929.jpg'
       },
@@ -86,7 +86,7 @@ module.exports = {
         lat: 33.23,
         lng: 36.69,
         name: 'Woodland retreat',
-        description: 'Welcome to our enchanting woodland retreat, nestled amidst a lush and serene forest setting! This cozy four-bedroom cabin offers a rustic yet modern escape, complete with panoramic forest views, a crackling fireplace, and a charming outdoor deck for savoring nature`s tranquility. Immerse yourself in the beauty of the surrounding woods, enjoy hiking trails right at your doorstep, and experience a peaceful getaway in this idyllic forest hideaway.',
+        description: 'Escape to a serene woodland hideaway! Cozy 4-bed cabin with panoramic forest views, crackling fireplace, and a charming outdoor deck. Embrace nature`s tranquility, explore nearby hiking trails, and relish a peaceful retreat in this enchanting forest getaway.',
         price: 200,
         previewImage: 'https://www.neighbor.com/storage-blog/wp-content/uploads/2020/03/AdobeStock_89298214-min_8421efb06b9d433a6f2f17d886703510_2000-550x367.jpeg'
       }
